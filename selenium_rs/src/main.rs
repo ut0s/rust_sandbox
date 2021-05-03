f39dcfa11017ebb64da6f044f3022ae8eca8df4d
Yuto TAGASHIRA <ut0s.dev@gmail.com>
2021-05-03 09:47:20 +0900

    feat: predict tile move and eval tile score

--- selenium_rs/src/main.rs
+++ selenium_rs/src/main.rs
@@ -1,10 +1,364 @@
+use rand::Rng;
+use std::slice::Iter;
 use std::time::Instant;
-use thirtyfour::prelude::*;
-use tokio;
+use thirtyfour_sync::prelude::*;
+// use thirtyfour::prelude::*;
+// use tokio;
 
 const HEADLESS_MODE: bool = true;
+const TILE_SIZE: usize = 4;
+// const DEPTH_COUNT: u32 = 1;
 
-async fn worker(is_headless: bool) -> WebDriverResult<()> {
+#[derive(Clone)]
+struct Tile {
+  pub number: u64,
+  // pub state: TileState,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+enum Direction {
+  Right,
+  Left,
+  Top,
+  Bottom,
+}
+
+impl Direction {
+  pub fn iterator() -> Iter<'static, Direction> {
+    static DIRECTIONS: [Direction; 4] = [
+      Direction::Right,
+      Direction::Left,
+      Direction::Top,
+      Direction::Bottom,
+    ];
+    DIRECTIONS.iter()
+  }
+}
+
+fn get_grid_info(tiles: &mut Vec<Vec<u64>>, line: String) {
+  let tmp: Vec<&str> = line.split(" ").collect();
+  // num
+  let tmp_num: Vec<&str> = tmp[1].split("-").collect();
+  let num: u64 = tmp_num[1].parse().unwrap();
+
+  // position
+  let tmp_pos: Vec<&str> = tmp[2].split("-").collect();
+  let x_pos: usize = tmp_pos[2].parse().unwrap();
+  let y_pos: usize = tmp_pos[3].parse().unwrap();
+
+  // if tmp.len() == 4 {
+  //   // state
+  //   let tmp_state: Vec<&str> = tmp[3].split("-").collect();
+  //   println!("{}", tmp_state[1]);
+  // }
+  // println!("tile num:{}", num);
+  // println!("x:{},y:{}", x_pos, y_pos);
+
+  tiles[y_pos - 1][x_pos - 1] = num;
+}
+
+fn predict(tiles: &Vec<Vec<u64>>) -> Direction {
+  let mut ans = 18;
+  let mut ret = Direction::Top;
+  for dir in Direction::iterator() {
+    let mut tiles_moved = tiles.clone();
+    next_tiles(&mut tiles_moved, *dir);
+    let tmp = eval_tiles(&tiles, &tiles_moved);
+
+    if ans > tmp {
+      ans = tmp;
+      ret = *dir;
+    }
+  }
+
+  // not update tiles
+  if ans == 18 {
+    ret = match rand::thread_rng().gen_range(0..5) {
+      0 => Direction::Top,
+      1 => Direction::Right,
+      2 => Direction::Left,
+      _ => Direction::Bottom,
+    };
+  }
+
+  ret
+}
+
+fn eval_tiles(tiles_org: &Vec<Vec<u64>>, tiles_moved: &Vec<Vec<u64>>) -> u32 {
+  if tiles_org == tiles_moved {
+    18
+  } else {
+    let mut c = 0;
+    for v in tiles_moved.iter() {
+      for e in v.iter() {
+        if *e != 0 {
+          c += 1;
+        }
+      }
+    }
+    c
+  }
+}
+
+fn next_tiles(tiles: &mut Vec<Vec<u64>>, dir: Direction) {
+  let rot_count: u32 = match dir {
+    Direction::Right => 2,
+    Direction::Left => 0,
+    Direction::Top => 3,
+    Direction::Bottom => 1,
+  };
+  rotate(tiles, rot_count);
+  tiles_to_left(tiles);
+  rotate(tiles, (4 - rot_count) % 4);
+}
+
+fn rotate(tiles: &mut Vec<Vec<u64>>, rot_count: u32) {
+  for _ in 0..rot_count {
+    tiles.reverse();
+    for i in 1..tiles.len() {
+      let (left, right) = tiles.split_at_mut(i);
+      for j in 0..i {
+        std::mem::swap(&mut left[j][i], &mut right[0][j]);
+      }
+    }
+  }
+}
+
+fn tiles_to_left(tiles: &mut Vec<Vec<u64>>) {
+  for row in 0..TILE_SIZE {
+    line_to_left(&mut tiles[row]);
+  }
+}
+
+fn line_to_left(l: &mut Vec<u64>) {
+  // remove zero
+  l.retain(|x| *x != 0);
+  for _ in 0..(TILE_SIZE - l.len()) {
+    l.push(0);
+  }
+
+  if l[0] == l[1] {
+    l.remove(0);
+    l[0] *= 2;
+    l.push(0);
+  }
+  if l[1] == l[2] {
+    l.remove(1);
+    l[1] *= 2;
+    l.push(0);
+  }
+  if l[2] == l[3] {
+    l.remove(2);
+    l[2] *= 2;
+    l.push(0);
+  }
+}
+
+#[cfg(test)]
+mod tests {
+  // Note this useful idiom: importing names from outer (for mod tests) scope.
+  use super::*;
+
+  #[test]
+  fn test00_line_to_left() {
+    let mut vec = vec![0, 0, 0, 0];
+    line_to_left(&mut vec);
+    assert_eq!(vec, [0, 0, 0, 0]);
+  }
+  #[test]
+  fn test01_line_to_left() {
+    let mut vec = vec![2, 2, 0, 0];
+    line_to_left(&mut vec);
+    assert_eq!(vec, [4, 0, 0, 0]);
+  }
+  #[test]
+  fn test02_line_to_left() {
+    let mut vec = vec![0, 2, 2, 0];
+    line_to_left(&mut vec);
+    assert_eq!(vec, [4, 0, 0, 0]);
+  }
+  #[test]
+  fn test03_line_to_left() {
+    let mut vec = vec![2, 0, 2, 0];
+    line_to_left(&mut vec);
+    assert_eq!(vec, [4, 0, 0, 0]);
+  }
+  #[test]
+  fn test04_line_to_left() {
+    let mut vec = vec![0, 0, 2, 2];
+    line_to_left(&mut vec);
+    assert_eq!(vec, [4, 0, 0, 0]);
+  }
+  #[test]
+  fn test05_line_to_left() {
+    let mut vec = vec![2, 4, 8, 8];
+    line_to_left(&mut vec);
+    assert_eq!(vec, [2, 4, 16, 0]);
+  }
+
+  #[test]
+  fn test00_rotate() {
+    let mut vec2d = vec![
+      vec![0, 1, 0, 0],
+      vec![0, 1, 0, 0],
+      vec![0, 1, 1, 1],
+      vec![0, 0, 0, 0],
+    ];
+    rotate(&mut vec2d, 0);
+    #[rustfmt::skip]
+    assert_eq!(
+      vec2d,
+      [[0, 1, 0, 0],
+       [0, 1, 0, 0],
+       [0, 1, 1, 1],
+       [0, 0, 0, 0]]
+    );
+  }
+  #[test]
+  fn test01_rotate() {
+    let mut vec2d = vec![
+      vec![0, 1, 0, 0],
+      vec![0, 1, 0, 0],
+      vec![0, 1, 1, 1],
+      vec![0, 0, 0, 0],
+    ];
+    rotate(&mut vec2d, 1);
+    #[rustfmt::skip]
+    assert_eq!(
+      vec2d,
+      [[0, 0, 0, 0],
+       [0, 1, 1, 1],
+       [0, 1, 0, 0],
+       [0, 1, 0, 0]]
+    );
+  }
+  #[test]
+  fn test02_rotate() {
+    let mut vec2d = vec![
+      vec![0, 1, 0, 0],
+      vec![0, 1, 0, 0],
+      vec![0, 1, 1, 1],
+      vec![0, 0, 0, 0],
+    ];
+    rotate(&mut vec2d, 4);
+    #[rustfmt::skip]
+    assert_eq!(
+      vec2d,
+      [[0, 1, 0, 0],
+       [0, 1, 0, 0],
+       [0, 1, 1, 1],
+       [0, 0, 0, 0]]
+    );
+  }
+
+  #[test]
+  fn test00_next_tiles() {
+    let mut vec2d = vec![
+      vec![0, 2, 0, 0],
+      vec![0, 2, 0, 0],
+      vec![0, 0, 2, 2],
+      vec![0, 0, 0, 0],
+    ];
+    next_tiles(&mut vec2d, Direction::Left);
+    #[rustfmt::skip]
+    assert_eq!(
+      vec2d,
+      [[2, 0, 0, 0],
+       [2, 0, 0, 0],
+       [4, 0, 0, 0],
+       [0, 0, 0, 0]]
+    );
+  }
+  #[test]
+  fn test01_next_tiles() {
+    let mut vec2d = vec![
+      vec![0, 2, 0, 0],
+      vec![0, 2, 0, 0],
+      vec![0, 0, 2, 2],
+      vec![0, 0, 0, 0],
+    ];
+    next_tiles(&mut vec2d, Direction::Right);
+    #[rustfmt::skip]
+    assert_eq!(
+      vec2d,
+      [[0, 0, 0, 2],
+       [0, 0, 0, 2],
+       [0, 0, 0, 4],
+       [0, 0, 0, 0]]
+    );
+  }
+  #[test]
+  fn test02_next_tiles() {
+    let mut vec2d = vec![
+      vec![0, 2, 0, 0],
+      vec![0, 2, 0, 0],
+      vec![0, 0, 2, 2],
+      vec![0, 0, 0, 0],
+    ];
+    next_tiles(&mut vec2d, Direction::Top);
+    #[rustfmt::skip]
+    assert_eq!(
+      vec2d,
+      [[0, 4, 2, 2],
+       [0, 0, 0, 0],
+       [0, 0, 0, 0],
+       [0, 0, 0, 0]]
+    );
+  }
+  #[test]
+  fn test03_next_tiles() {
+    let mut vec2d = vec![
+      vec![0, 2, 0, 0],
+      vec![0, 2, 0, 0],
+      vec![0, 0, 2, 2],
+      vec![0, 0, 0, 0],
+    ];
+    next_tiles(&mut vec2d, Direction::Bottom);
+    #[rustfmt::skip]
+    assert_eq!(
+      vec2d,
+      [[0, 0, 0, 0],
+       [0, 0, 0, 0],
+       [0, 0, 0, 0],
+       [0, 4, 2, 2]]
+    );
+  }
+
+  #[test]
+  fn test00_eval_tiles() {
+    let vec2d = vec![
+      vec![0, 2, 0, 0],
+      vec![0, 2, 0, 0],
+      vec![0, 0, 2, 2],
+      vec![0, 0, 0, 0],
+    ];
+    assert_eq!(eval_tiles(&vec2d), 4);
+  }
+
+  #[test]
+  fn test00_predict() {
+    let vec2d = vec![
+      vec![0, 2, 0, 0],
+      vec![0, 2, 0, 0],
+      vec![0, 0, 4, 2],
+      vec![0, 0, 0, 0],
+    ];
+    assert_eq!(predict(&vec2d), Direction::Top);
+  }
+
+  #[test]
+  fn test01_predict() {
+    let vec2d = vec![
+      vec![0, 2, 0, 0],
+      vec![0, 0, 0, 0],
+      vec![0, 0, 4, 2],
+      vec![0, 0, 0, 0],
+    ];
+    assert_eq!(predict(&vec2d), Direction::Right);
+  }
+}
+
+fn worker(is_headless: bool) -> WebDriverResult<()> {
   //start timer
   let start = Instant::now();
 
@@ -12,20 +366,59 @@ async fn worker(is_headless: bool) -> WebDriverResult<()> {
   if is_headless {
     let _ = caps.set_headless();
   }
-  let driver = WebDriver::new("http://localhost:4444", &caps).await?;
+  let driver = WebDriver::new("http://localhost:4444", &caps)?;
 
-  // Navigate to https://wikipedia.org.
-  driver.get("https://play2048.co").await?;
-  let html_body = driver.find_element(By::Tag("body")).await?;
+  driver.get("https://play2048.co")?;
+  let html_body = driver.find_element(By::Tag("body"))?;
 
   // restart
-  html_body.send_keys('r').await?;
+  // html_body.send_keys('r')?;
 
-  for _ in 0..100 {
-    html_body.send_keys(Keys::Up).await?;
-    html_body.send_keys(Keys::Right).await?;
-    html_body.send_keys(Keys::Down).await?;
-    html_body.send_keys(Keys::Left).await?;
+  let grid = driver.find_element(By::ClassName("tile-container"))?;
+  std::thread::sleep(std::time::Duration::from_millis(500));
+
+  let mut check_stuck = 0;
+  let mut old_tiles = vec![vec![0; TILE_SIZE]; TILE_SIZE];
+  for step in 0..1000 {
+    // println!("\nStep:{}", step);
+
+    // init tile
+    let mut tiles = vec![vec![0; TILE_SIZE]; TILE_SIZE];
+
+    let tile_list = grid.find_elements(By::ClassName("tile"))?;
+    // std::thread::sleep(std::time::Duration::from_millis(50));
+    for tile in tile_list {
+      let oneline = tile.class_name()?.unwrap();
+      // println!("{:?}", oneline);
+      get_grid_info(&mut tiles, oneline);
+    }
+    if tiles == old_tiles {
+      check_stuck += 1;
+      if check_stuck > 10 {
+        for i in 0..tiles.len() {
+          println!("{:?}", tiles[i]);
+        }
+        break;
+      }
+    } else {
+      check_stuck = 0;
+    }
+
+    let key: Direction = predict(&tiles);
+    let _ = match key {
+      Direction::Right => html_body.send_keys(Keys::Right),
+      Direction::Left => html_body.send_keys(Keys::Left),
+      Direction::Top => html_body.send_keys(Keys::Up),
+      Direction::Bottom => html_body.send_keys(Keys::Down),
+    };
+    std::thread::sleep(std::time::Duration::from_millis(15));
+    old_tiles = tiles.clone();
+
+    // for i in 0..tiles.len() {
+    //   println!("{:?}", tiles[i]);
+    // }
+    // println!("{:?}", key);
+    // println!("Stuck Count:{}", check_stuck);
   }
 
   //end timer
@@ -40,12 +433,12 @@ async fn worker(is_headless: bool) -> WebDriverResult<()> {
   let start = Instant::now();
 
   let mut tiles_vec = Vec::new();
-  let grid = driver.find_element(By::ClassName("tile-container")).await?;
-  for tile in grid.find_elements(By::ClassName("tile")).await? {
-    // println!("{:?}", tile.class_name().await?);
-    // println!("{}", tile.text().await?);
-    // tiles_vec.push(tile.text().await?);
-    let tmp_num: i32 = tile.text().await?.parse().unwrap();
+  let grid = driver.find_element(By::ClassName("tile-container"))?;
+  for tile in grid.find_elements(By::ClassName("tile"))? {
+    // println!("{:?}", tile.class_name()?);
+    // println!("{}", tile.text()?);
+    // tiles_vec.push(tile.text()?);
+    let tmp_num: i32 = tile.text()?.parse().unwrap();
     tiles_vec.push(tmp_num);
   }
   //end timer
@@ -56,22 +449,16 @@ async fn worker(is_headless: bool) -> WebDriverResult<()> {
     end.subsec_nanos() / 1_000_000
   );
 
-  // println!("{:?}", tiles_vec);
   tiles_vec.sort_unstable();
-  println!("{:?}", tiles_vec);
+  // println!("{:?}", tiles_vec);
   println!("max num of tile: {}", tiles_vec[tiles_vec.len() - 1]);
 
-  let score = driver
-    .find_element(By::ClassName("score-container"))
-    .await?;
-  println!("{}", score.text().await?);
+  let score = driver.find_element(By::ClassName("score-container"))?;
+  println!("{}", score.text()?);
 
   Ok(())
 }
 
-#[tokio::main]
-async fn main() {
-  let _1 = worker(HEADLESS_MODE).await;
-  // let _2 = worker(HEADLESS_MODE).await;
-  // let _3 = worker(HEADLESS_MODE).await;
+fn main() {
+  let _1 = worker(HEADLESS_MODE);
 }
